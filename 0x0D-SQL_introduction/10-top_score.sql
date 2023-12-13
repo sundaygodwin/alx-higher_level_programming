18f1bc918b7a1a5c4c513126786a8bc988567df6
sundaygodwin <godwinsunday194@gmail.com>
2023-12-13 12:20:05 -0800

    correction

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,6 @@
---Script: Lists all records of the table second_table.
+-- Script: Lists all records of the table second_table.
 -- Records are ordered by descending score.
+USE hbtn_0c_0;
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
