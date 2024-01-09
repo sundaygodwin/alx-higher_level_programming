5fb8b320880ce209c43b7c21605e69ccf34c3c78
Geexix <gift85210@gmail.com>
2024-01-09 22:50:45 +0100

    correction

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-let C ='C is fun';
-let python = 'Python is cool';
-let Js = 'JavaScript is amazing';
-
-console.log(C);
-console.log(python);
-console.log(Js);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i=0; i < myArr.length; i++ ) {
-  console.log(myArr[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
